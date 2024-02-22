88138d3dc6555e1fad1bd934845ba41664c8fd9d
Jake Robins <me@jakerobins.com>
2024-02-22 11:25:26 -0600

    scaffold

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        // Add your components here
+      },
+      apiOptions: {
+        region: "us",
+      },
+    }),
+  ],
+});
