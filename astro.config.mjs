d5661eff42d725d2c56f9b910a7f0d5b9528fae1
Jake Robins <me@jakerobins.com>
2024-04-23 09:40:38 -0600

    add sitemap, redo contact

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
 import { siteConfig } from "./src/config/config";
+import sitemap from "@astrojs/sitemap";
 
 const env = loadEnv("", process.cwd(), "STORYBLOK");
 
@@ -17,6 +18,14 @@ export default defineConfig({
       },
       apiOptions: {
         region: "us",
+      },
+    }),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-CA",
+        },
       },
     }),
   ],
